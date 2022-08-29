452da8c5ee23677d04fa639deb2c1620d4c7b7da
Your Name <you@example.com>
2022-08-29 14:56:26 +0530

    second commit package change

---
+++ app/build/generated/source/buildConfig/debug/com/rkrocks/bricksbeaker/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.rkrocks.bricksbeaker;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.rkrocks.bricksbeaker";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final long TIMESTAMP = 1661764895464L;
+}
